be43ec6e5878784d9b28d2f981615a607401c10b
bloodlaac <anaevaulia5@gmail.com>
2026-03-23 18:00:51 +0400

    add dependencies

---
+++ src/main/java/ru/yanaeva/sniffnet_cw/SniffnetCwApplication.java
@@ -0,0 +1,13 @@
+package ru.yanaeva.sniffnet_cw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SniffnetCwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SniffnetCwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yanaeva/sniffnet_cw/SniffnetCwApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yanaeva.sniffnet_cw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SniffnetCwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
