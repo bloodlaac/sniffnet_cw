472809e76db079c43c91e5940ac08d7104e18a46
bloodlaac <anaevaulia5@gmail.com>
2026-04-17 16:12:40 +0400

    delete stuff

--- backend/src/main/java/ru/yanaeva/sniffnet_cw/controller/DatasetController.java
+++ backend/src/main/java/ru/yanaeva/sniffnet_cw/controller/DatasetController.java
@@ -1,6 +1,5 @@
 package ru.yanaeva.sniffnet_cw.controller;
 
-import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
